a6de89951102248fa6e22e45204c1a11c633960d
MrSpaar <julienpistre.cagnes@gmail.com>
2022-06-01 23:07:58 +0200

    Update JdP

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Bare de navigation responsive
 function myFunction() {
-    var x = document.getElementById("myTopnav");
+    let x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {
@@ -29,15 +29,14 @@ function copy() {
 }
 
 function setStaffSkins() {
-    url = "https://crafatar.com/avatars/";
+    let url = "https://crafatar.com/avatars/";
+    let ids = [
+        "8406c95d235f4aa98be7798ad7ca9e37", "2f756bae16fe412c8f74baf0907dc25d", "335d6b831f2d4f19a280f329a928c692",
+        "b29e19ca633e3ea4bfe1d9208a95178d", "406054323fbe4e49bc54f7fb0ae3c818", "5a48464a-2376-3e9f-b1e8-c8dde6d9fd3f",
+    ];
 
-    var skins = [];
-    var ids = ["8406c95d235f4aa98be7798ad7ca9e37", "2f756bae16fe412c8f74baf0907dc25d", "335d6b831f2d4f19a280f329a928c692", "406054323fbe4e49bc54f7fb0ae3c818", "5a48464a-2376-3e9f-b1e8-c8dde6d9fd3f"];
-
-    for (id of ids) 
-
-    for (let i = 1; i <= ids.length; i++) {
-        document.getElementById(`staff${i}`).src = url + ids[i-1];
+    for (let i = 0; i <= ids.length; i++) {
+        document.getElementById(`staff${i+1}`).src = url + ids[i];
     }
 }
 
