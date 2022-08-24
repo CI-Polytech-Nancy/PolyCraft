dde4e5e90ce60b30f1894c98ecd00ba001ad5587
mrspaar <julienpistre.cagnes@gmail.com>
2022-08-24 14:47:18 +0200

    Removed Bootstrap & Switched to SASS

---
+++ static/script.js
@@ -0,0 +1,19 @@
+// Afficher ou pas le bouton pour retourner en haut de la page
+window.onscroll = function () {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("topBtn").style.opacity = "1";
+    } else {
+        document.getElementById("topBtn").style.opacity = "0";
+    }
+}
+
+// Retourner en haut de la page
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+// Copier l'addresse IP du serveur
+function copyId() {
+    navigator.clipboard.writeText("mc.polycraft.fr");
+}
